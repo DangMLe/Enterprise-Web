b0168286f83e45cdba03f077ed858def62fb60a0
Jasmine <30364033+nguyetvo@users.noreply.github.com>
2021-04-03 15:44:05 +0700

    Update Comment.php

---
+++ app/Controllers/Comment.php
@@ -0,0 +1,20 @@
+public function add($post_id) {
+    // if nothing posted redirect
+    if (!$this->input->post()) {
+        redirect(site_url());
+    }
+
+    // TODO: save comment in database
+    $this->form_validation->set_rules($this->comment_model->rules);
+    if ($this->form_validation->run() == true) {
+        // Store the success message in flash data
+        $this->session->set_flashdata('message', 'Ok! TODO save the comment.');
+        // Redirect back to posts page
+        redirect('posts/'.$post_id, 'refresh');
+    } else {
+        // Store the error message in flash data
+        $this->session->set_flashdata('message', validation_errors());
+        // Redirect back to posts page
+        redirect('posts/'.$post_id, 'refresh');
+    }
+}
