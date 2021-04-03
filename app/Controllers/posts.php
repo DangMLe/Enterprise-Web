69a4e310a7408317a49a0bae14eb88bdaf707b17
Jasmine <30364033+nguyetvo@users.noreply.github.com>
2021-04-03 15:45:29 +0700

    Create posts.php

---
+++ app/Controllers/posts.php
@@ -0,0 +1,7 @@
+public function index($post_id) {
+    $this->data['message'] = $this->session->flashdata('message');
+    $this->load->view('posts', $this->data);
+}
+
+--echo $message; 
+--post view
